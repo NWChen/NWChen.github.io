6e127cf26794399ab8302bbc3862330a088e1969
Neil Chen <neilwchen@gmail.com>
2017-02-04 09:59:28 -0500

    Add g a b

--- script.js
+++ script.js
@@ -4,12 +4,25 @@ var alpha = 0;
 var beta = 0;
 var gamma = 0;
 
+
 function handleOrientation(event) {
 	var absolute = event.absolute;
 	
 	if(event.alpha != alpha) {
 		alpha = event.alpha;
-		document.getElementById('gyro').innerHTML = alpha;
+		document.getElementById('alpha').innerHTML = alpha;
 		console.log(alpha);	
+	}
+
+	if(event.beta != beta) {
+		beta = event.beta;
+		document.getElementById('beta').innerHTML = beta;
+		console.log(beta);
+	}	
+
+	if(event.gamma != gamma) {
+		gamma = event.gamma;
+		document.getElementById('gamma').innerHTML = gamma;
+		console.log(gamma);
 	}
 }
